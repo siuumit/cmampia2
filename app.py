6bc25f786891c1a8098de451f62a00c998774e62
siuumit <sum.cha.rt22@dypatil.edu>
2025-04-21 22:25:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ with open('tfidf.pkl', 'rb') as f:
 with open('clf.pkl', 'rb') as f:
     clf = pickle.load(f)
 
-# Connect to MongoDB Atlas
-MONGO_URI = os.getenv('MONGO_URI')  # Ensure this is set in Render's environment settings
+# Connect to MongoDB Atlas using environment variable
+MONGO_URI = os.getenv('MONGO_URI')
 client = MongoClient(MONGO_URI)
 db = client['sentimentDB']
 collection = db['predictions']
@@ -26,9 +26,9 @@ def predict():
     if request.method == 'POST':
         message = request.form['message']
         transformed_input = tfidf.transform([message])
-        prediction = clf.predict(transformed_input)[0]  # 1 = positive, 0 = negative
+        prediction = clf.predict(transformed_input)[0]
 
-        # Save to MongoDB
+        # Save the prediction to MongoDB
         collection.insert_one({
             'text': message,
             'prediction': int(prediction)
@@ -36,7 +36,8 @@ def predict():
 
         sentiment = int(prediction)
 
-    return render_template('index.html', sentiment=sentiment, message=message)
+    return render_template('index.html', sentiment=sentiment, comment=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
